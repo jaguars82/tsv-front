de49f01ec1438ccaf8274ec5fde8c1e6fe4d2141
Egor Sechin <jaguars82@yandex.ru>
2021-03-17 00:26:26 +0300

    install axios, test backend

--- src/components/Carcas.vue
+++ src/components/Carcas.vue
@@ -12,8 +12,28 @@ export default {
   data () {
     return {
     }
+  },
+  beforeCreate () {
+    this.axios({
+      method: 'get',
+      url: "http://test.teacher-info.net/vserver/server.php?queryMode=develop&table=razdeli&fields=id, name, real_name, access, default_content, navigation_mode, allow_mode_change&condition=`parent` = 'root' AND `hidden` = '0'&order=order",
+      /*
+      method: 'post',
+      url: '/vserver/server.php',
+      data: {
+        table: 'razdeli',
+        fields: ['id', 'name', 'real_name', 'access', 'default_content', 'navigation_mode', 'allow_mode_change'],
+        condition: []
+      }
+      */
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
-
 }
 </script>
 
