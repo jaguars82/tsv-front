7e0e803c9ddbbffba4bcafdbccdce1e3e5b465a0
Egor Sechin <jaguars82@yandex.ru>
2021-05-07 01:36:55 +0300

    main menu add toolbar component

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,20 +1,48 @@
 <template>
-  <div>
-    Main Menu
+  <q-toolbar class="bg-primary text-white">
+    <q-btn
+      v-for="item in mainMenu"
+      :key="item.id"
+      :label="item.real_name"
+      :to="'/'+item.name"
+      stretch
+      flat
+    >
+
+    </q-btn>
+    <!--Main Menu
     <ul>
       <li v-for="item in mainMenu" :key="item.id"><router-link :to="'/'+item.name">{{ item.real_name }}</router-link></li>
     </ul>
-  </div>
+    -->
+  </q-toolbar>
 </template>
 
 <script>
 
   export default {
+    data () {
+      return {
+        // windowWidth: 0
+      }
+    },
     computed: {
       mainMenu () {
         return this.$store.getters.mainMenu
       }
-    }
+    }/* ,
+    created() {
+      window.addEventListener('resize', this.handleResize);
+      this.handleResize();
+    },
+    destroyed() {
+      window.removeEventListener('resize', this.handleResize);
+    },
+    methods: {
+      handleResize() {
+        this.windowWidth = window.innerWidth;
+      }
+    } */
   }
 
 </script>
